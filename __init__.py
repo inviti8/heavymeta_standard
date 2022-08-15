5d6de6197081ce8d9b47f6e83e1f64752580e7ab
Jason <dimensions3@outlook.com>
2022-08-14 21:38:31 -0700

    Adding trait buttons

--- __init__.py
+++ __init__.py
@@ -43,9 +43,32 @@ bl_info = {
 import bpy
 import re
 from rna_prop_ui import PropertyPanel
-from bpy.types import (Panel, Collection)
+from bpy.types import (Panel,
+                       BoolProperty,
+                       StringProperty,
+                       EnumProperty,
+                       CollectionProperty,
+                       Collection)
 
+from bpy.types import (Operator,
+                       Panel,
+                       PropertyGroup,
+                       UIList)
+
+# -------------------------------------------------------------------
+#   Heavymeta Standards Panel
+# -------------------------------------------------------------------
 PROPS = [
+    ('minter_type', bpy.props.EnumProperty(
+        name='Minter-Type',
+        items=(
+            ('HVYC', "Character", ""),
+            ('HVYI', "Immortal", ""),
+            ('HVYA', "Animal", ""),
+            ('HVYW', "Weapon", ""),
+            ('HVYO', "Object", ""),
+            ('HVYG', "Generic", ""),
+            ('HVYAU', "Auricle", "")))),
     ('minter_name', bpy.props.StringProperty(name='Minter-Name', default='')),
     ('minter_description', bpy.props.StringProperty(name='Minter-Description', default='')),
     ('minter_image', bpy.props.StringProperty(name='Minter-Image', subtype='FILE_PATH', default='')),
@@ -56,6 +79,29 @@ PROPS = [
 class MetaTrait(bpy.types.PropertyGroup):
     trait_type = bpy.props.StringProperty()
     value = bpy.props.StringProperty()
+
+# ------------------------------------------------------------------------
+#    Heavymeta Operators
+# ------------------------------------------------------------------------
+
+class OpAddTrait(bpy.types.Operator):
+    """Print object name in Console"""
+    bl_idname = "collection.add_trait_operator"
+    bl_label = "Add Trait Operator"
+
+    def execute(self, context):
+        print (context.collection)
+        return {'FINISHED'}
+
+
+class OpRemoveTrait(bpy.types.Operator):
+    """Print object name in Console"""
+    bl_idname = "collection.remove_trait_operator"
+    bl_label = "Remove Trait Operator"
+
+    def execute(self, context):
+        print (context.collection)
+        return {'FINISHED'}
 
 class HeavymetaStandardDataPanel(bpy.types.Panel):
     """Creates a Panel in the Object properties window"""
@@ -69,11 +115,20 @@ class HeavymetaStandardDataPanel(bpy.types.Panel):
         col = self.layout.column()
         for (prop_name, _) in PROPS:
             row = col.row()
-            if prop_name == 'version':
+            if prop_name == 'minter_version':
                 row = row.row()
                 row.enabled = context.collection.add_version
             row.prop(context.collection, prop_name)
+        row = col.row()
+        row.separator()
+        row.label(text="Traits:")
+        row.operator(OpAddTrait.bl_idname, text="Add", icon="ADD")
+        row.operator(OpRemoveTrait.bl_idname, text="Remove", icon="REMOVE")
 
+
+# -------------------------------------------------------------------
+#   Custom Properties Panel
+# -------------------------------------------------------------------
 class CollectionButtonsPanel:
     bl_space_type = 'PROPERTIES'
     bl_region_type = 'WINDOW'
@@ -88,6 +143,8 @@ def register():
     for (prop_name, prop_value) in PROPS:
         setattr(bpy.types.Collection, prop_name, prop_value)
 
+    bpy.utils.register_class(OpAddTrait)
+    bpy.utils.register_class(OpRemoveTrait)
     bpy.utils.register_class(COLLECTION_PT_collection_custom_props)
     bpy.utils.register_class(HeavymetaStandardDataPanel)
 
@@ -96,6 +153,8 @@ def unregister():
     for (prop_name, _) in PROPS:
         delattr(bpy.types.Collection, prop_name)
 
+    bpy.utils.unregister_class(OpAddTrait)
+    bpy.utils.unregister_class(OpRemoveTrait)
     bpy.utils.unregister_class(COLLECTION_PT_collection_custom_props)
     bpy.utils.unregister_class(HeavymetaStandardDataPanel)
 
